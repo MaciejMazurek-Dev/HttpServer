e0c208a6c7a92932a6baa5b6e913c0cb9db7599c
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-02-24 16:26:28 +0100

    Add constructors for TcpServer

---
+++ src/HttpServer/Services/TcpServer.cs
@@ -0,0 +1,24 @@
+﻿using HttpServer.Utilities;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HttpServer.Services
+{
+    public class TcpServer
+    {
+        private readonly TcpListener _tcpListener;
+        public TcpServer()
+        {
+            IPEndPoint ipEnDPoint = new(IPAddress.Loopback, 80);
+            _tcpListener = new(ipEnDPoint);
+        }
+        public TcpServer(string ipAddress, int portNumber)
+        {
+            IpToByteConverter converter = new();
+            byte[] ipAddressInBytes = converter.ConvertIpV4ToByteArray(ipAddress);
+            IPAddress ip = new(ipAddressInBytes);
+            IPEndPoint ipEndPoint = new(ip, portNumber);
+            _tcpListener = new(ipEndPoint);
+        }
+    }
+}
