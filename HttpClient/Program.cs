107ade7822361162303d6fc306eb7164444a374d
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-03-04 16:44:12 +0100

    Add basic HTTP client

---
+++ HttpClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using HttpClient.Services;
+
+namespace HttpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpSocketClient client = new("127.0.0.1", 80);
+
+            client.StartClient();
+        }
+    }
+}
---
+++ HttpClient/Services/TcpSocketClient.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HttpClient.Services
+{
+    public class TcpSocketClient
+    {
+        private readonly Socket _socket;
+        private readonly IPEndPoint _serverIpEndPoint;
+        public TcpSocketClient(string serverIp, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(serverIp);
+            _serverIpEndPoint = new(ipAddress, port);
+            _socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void StartClient()
+        {
+            _socket.Connect(_serverIpEndPoint);
+            Console.WriteLine("Connection with server established.");
+            while (true)
+            {
+                Console.WriteLine("Write a message:");
+                string message = Console.ReadLine();
+                byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+                int bytesSend = _socket.Send(messageBytes);
+
+                Console.WriteLine($"Message send - {bytesSend} bytes.");
+                ReceiveResponse();
+            }
+        }
+
+        public void ReceiveResponse()
+        {
+            byte[] buffer = new byte[1024];
+            _socket.ReceiveBufferSize = buffer.Length;
+            int bytesReceived = _socket.Receive(buffer);
+            if (bytesReceived > 0)
+            {
+                string received = Encoding.ASCII.GetString(buffer);
+                Console.WriteLine($"Server response: {received}");
+            }
+        }
+    }
+}
