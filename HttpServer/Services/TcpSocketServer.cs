0305b99d075e7b768e6d7a034f61d01584aaf403
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-03-11 16:08:46 +0100

    Add logger to TcpSocketServer

--- HttpServer/Services/TcpSocketServer.cs
+++ HttpServer/Services/TcpSocketServer.cs
@@ -1,33 +1,47 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace HttpServer.Services
 {
     public class TcpSocketServer
     {
-        private readonly Socket _socket = new(
+        private readonly Socket _serverSocket = new(
                                         AddressFamily.InterNetwork,
                                         SocketType.Stream,
                                         ProtocolType.Tcp);
-        private readonly IPEndPoint _serverIpEndPoint;
         private bool isRunning = false;
-        public TcpSocketServer() : this(IPAddress.Loopback.ToString(), 80)
+        private readonly ILogger _logger;
+
+        public TcpSocketServer() : this(IPAddress.Loopback.ToString(), 5050)
         {
         }
         public TcpSocketServer(string ipString, int port)
         {
             IPAddress ipAddress = IPAddress.Parse(ipString);
-            _serverIpEndPoint = new(ipAddress, port);
-            _socket.Bind(_serverIpEndPoint);
-            _socket.Listen(10);
+            IPEndPoint serverIpEndPoint = new(ipAddress, port);
+            _serverSocket.Bind(serverIpEndPoint);
+            _serverSocket.Listen(10);
+
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Debug);
+                builder.AddConsole();
+            });
+            _logger = loggerFactory.CreateLogger("TcpSocketServer");
         }
+
 
         public void StartServer()
         {
-            Socket connectionHandler = _socket.Accept();
+            _logger.LogDebug("Logger initialized.");
+
+            Socket connectionHandler = _serverSocket.Accept();
             isRunning = true;
-            Console.WriteLine("Server is running...");
+
+            _logger.LogInformation("Server state: UP");
+            _logger.LogInformation($"Listening for connections on address: {connectionHandler.LocalEndPoint}");
             while (isRunning)
             {
                 byte[] buffer = new byte[1024];
@@ -35,9 +49,11 @@ namespace HttpServer.Services
                 if (bytesCount > 0)
                 {
                     string received = Encoding.ASCII.GetString(buffer);
-                    Console.WriteLine($"Bytes received: {bytesCount}");
-                    Console.WriteLine($"Message: {received}");
+
+                    _logger.LogDebug($"Data received from: {connectionHandler.RemoteEndPoint}\n{received}");
                 }
+
+
                 string responseMessage = $"Message received - {bytesCount} bytes";
                 byte[] response = Encoding.ASCII.GetBytes(responseMessage);
                 connectionHandler.Send(response);
@@ -47,7 +63,7 @@ namespace HttpServer.Services
         public void StopServer()
         {
             isRunning = false;
-            _socket.Shutdown(SocketShutdown.Both);
+            _serverSocket.Shutdown(SocketShutdown.Both);
             Console.WriteLine("Server shutdown...");
         }
     }
